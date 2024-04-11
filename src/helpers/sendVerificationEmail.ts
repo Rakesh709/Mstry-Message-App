fd96287eaa78af516fb3af333bcd6151a8b483b4
Rakesh709 <kumar.rakesh1997@outlook.com>
2024-04-11 22:57:16 +0530

    Setup Resend email with NextJS

---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,29 @@
+import { resend } from "@/lib/resend";
+
+import {VerificationEmail} from "../../emails/VerificationEmail";
+import { types } from "util";
+
+
+import {ApiResponse} from "@/types/ApiResponse";
+import { promises } from "dns";
+import { verify } from "crypto";
+
+export async function sendVerificationEmail(
+    email: string,
+    username: string,
+    verification: string
+):promise<ApiResponse>{
+    try{
+        await resend.emails.send({
+            from: 'onboarding@resend.dev',
+            to: email,
+            subject: Mystry message | Verification Code',
+            react: VerificationEmail({username,otp:verifyCode}),
+          });
+        return{success:true,message:" Verification email send successfully"}
+
+    } catch (emailError){
+        console.log("Error sending verification email",emailError)
+        return{success:false,message:"Failed to send verification email"}
+    }
+}
