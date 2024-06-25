2c9e7c0cd351e97cc3ccc9bb1e2172703fd71b11
Rakesh709 <kumar.rakesh1997@outlook.com>
2024-06-25 23:24:19 +0530

    OTP verification and unique username check

--- src/app/api/check-username-unique/route.ts
+++ src/app/api/check-username-unique/route.ts
@@ -1,3 +1,20 @@
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
 import { z } from "zod";
+//whenever you are taking zod you need schema from somewhere in this case signupschema,ts file 
+
+import {usernameValidation} from "@/schemas/signUpSchema";
+
+import {signUpSchema} from "@/schemas/signUpSchema"
+
+//query schemacreating
+
+const UsernameQuerySchema = z.object({
+    username: usernameValidation
+})
+
+//now make get call and checking it
+
+export async function GET(request:Request){
+    await dbConnect()
+}
