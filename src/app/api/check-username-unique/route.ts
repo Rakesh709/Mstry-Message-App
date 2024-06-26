e3280821af8346f04e048b7a1644aa88f3ba36cd
Rakesh709 <kumar.rakesh1997@outlook.com>
2024-06-26 22:17:31 +0530

    unique username checking

--- src/app/api/check-username-unique/route.ts
+++ src/app/api/check-username-unique/route.ts
@@ -17,4 +17,40 @@ const UsernameQuerySchema = z.object({
 
 export async function GET(request:Request){
     await dbConnect()
+
+    //below will be the example of the url from url find the username
+    //localhost:3000/api/checkuserunique?username=rakesh&phone=android
+    try {
+        const {searchParams}= new URL(request.url)
+        //below is the syntax
+        const queryParam ={
+            username: searchParams.get("username")
+        }
+
+        //validate username with zod
+        const result = UsernameQuerySchema.safeParse(queryParam)
+        console.log(result) //todo remove
+
+
+        if (!result.success) {
+            const usernameErrors= result.error.format().username?._errors
+              ||  []
+            return Response.json({
+                success:false,
+                message: "Invalid query parameters"
+            },{
+                status:400
+            })
+            }
+
+
+    } catch (error) {
+        console.error("Error checking username",error)
+        return Response.json({
+            success:false,
+            message: "Error checking username"
+        },{
+            status:500
+        })
+    }
 }
