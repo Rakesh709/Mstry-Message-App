b8cf1f6fb8e4de29112a0ef6139a90151ebedb30
Rakesh709 <kumar.rakesh1997@outlook.com>
2024-07-14 23:54:40 +0530

    sign-in

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,9 +1,137 @@
-import React from 'react'
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { useDebounceCallback } from "usehooks-ts";
+import { useToast } from "@/components/ui/use-toast";
+import { useRouter } from "next/navigation";
+import { signUpSchema } from "@/schemas/signUpSchema";
+import axios, { AxiosError } from "axios";
+import { ApiResponse } from "@/types/ApiResponse";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
+import { signInSchema } from "@/schemas/signInSchema";
+import { signIn } from "next-auth/react";
 
 function page() {
+  
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+ 
+  //toast
+  const { toast } = useToast();
+
+  //router navigation
+  const router = useRouter();
+
+  //zod implementation
+  const form = useForm({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      //later you can add more input form
+    },
+  });
+
+
+
+  //submit method
+  //below is used in industry
+  const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+    
+    //sign in using nextAOuth
+    await signIn('credentials',{
+      identifier: data.identifier,
+      password: data.password
+    })
+
+  };
+
   return (
-    <div>Sign-in page</div>
-  )
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className=" w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Join Mystery Message
+          </h1>
+          <p className="mb-4">Sign up to start your anonymous adventure </p>
+        </div>
+       
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            
+            
+            <FormField
+              name="identifier"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="email" 
+                    //need to insert value into field
+                    {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="password" 
+                    //need to insert value into field
+                    {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+           
+            <Button type="submit" disabled={isSubmitting}>
+                {
+                  isSubmitting ? (
+                      <>
+                      <Loader2 className="mr-2 h-4 w-4 animate-spin"/> Please wait
+                      </>
+
+                  ) : ('SignUp')
+                }
+            </Button>
+          </form>
+        </Form>
+        <div className="text-center mt-4">
+                <p>
+                  Already a member?{''}
+                  <Link href='/sign-in' className="text-blue-600 hover:text-blue-800" >Sign in</Link>
+                </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default page
+//tom import something from suggestion ctrl+ space
+export default page;
