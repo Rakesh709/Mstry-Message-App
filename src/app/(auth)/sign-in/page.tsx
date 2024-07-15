ad55abcdab37abe8c7ebe0b24846999293a20cdf
Rakesh709 <kumar.rakesh1997@outlook.com>
2024-07-15 23:01:19 +0530

    sign-in completed

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -28,9 +28,7 @@ import { signIn } from "next-auth/react";
 
 function page() {
   
-  const [isSubmitting, setIsSubmitting] = useState(false);
 
- 
   //toast
   const { toast } = useToast();
 
@@ -41,24 +39,32 @@ function page() {
   const form = useForm({
     resolver: zodResolver(signInSchema),
     defaultValues: {
-      email: "",
+      identifier: "",
       password: "",
       //later you can add more input form
     },
   });
 
-
-
   //submit method
   //below is used in industry
   const onSubmit = async (data: z.infer<typeof signInSchema>) => {
-    
     //sign in using nextAOuth
-    await signIn('credentials',{
+    const result = await signIn("credentials", {
+      redirect: false,
       identifier: data.identifier,
-      password: data.password
-    })
+      password: data.password,
+    });
+    if (result?.error) {
+      toast({
+        title: "Login Failed",
+        description: "Incorrect username or password",
+        variant: "destructive",
+      });
+    }
 
+    if(result?.url){
+      router.replace('/dashboard')
+    }
   };
 
   return (
@@ -68,23 +74,23 @@ function page() {
           <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
             Join Mystery Message
           </h1>
-          <p className="mb-4">Sign up to start your anonymous adventure </p>
+          <p className="mb-4">Sign in to start your anonymous adventure </p>
         </div>
-       
+
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-            
-            
+
             <FormField
               name="identifier"
               control={form.control}
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel>Email</FormLabel>
+                  <FormLabel>Email/Username</FormLabel>
                   <FormControl>
-                    <Input placeholder="email" 
-                    //need to insert value into field
-                    {...field}
+                    <Input
+                      placeholder="email/username"
+                      //need to insert value into field
+                      {...field}
                     />
                   </FormControl>
                   <FormMessage />
@@ -92,7 +98,6 @@ function page() {
               )}
             />
 
-            
             <FormField
               name="password"
               control={form.control}
@@ -100,33 +105,30 @@ function page() {
                 <FormItem>
                   <FormLabel>Password</FormLabel>
                   <FormControl>
-                    <Input type="password" placeholder="password" 
-                    //need to insert value into field
-                    {...field}
+                    <Input
+                      type="password"
+                      placeholder="password"
+                      //need to insert value into field
+                      {...field}
                     />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
               )}
             />
-           
-            <Button type="submit" disabled={isSubmitting}>
-                {
-                  isSubmitting ? (
-                      <>
-                      <Loader2 className="mr-2 h-4 w-4 animate-spin"/> Please wait
-                      </>
 
-                  ) : ('SignUp')
-                }
+            <Button type="submit">
+              Signin
             </Button>
           </form>
         </Form>
         <div className="text-center mt-4">
-                <p>
-                  Already a member?{''}
-                  <Link href='/sign-in' className="text-blue-600 hover:text-blue-800" >Sign in</Link>
-                </p>
+          <p>
+            Not a member?{""}
+            <Link href="/sign-up" className="text-blue-600 hover:text-blue-800">
+              Sign up
+            </Link>
+          </p>
         </div>
       </div>
     </div>
