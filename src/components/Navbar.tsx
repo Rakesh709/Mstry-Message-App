712b6c27cd6da43579ebba754b92ebe1f59e56a3
Rakesh709 <kumar.rakesh1997@outlook.com>
2024-07-18 22:48:23 +0530

    navbar completed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import Link from 'next/link'
 import { useSession , signOut} from 'next-auth/react'
 import {User} from 'next-auth'
+import { Button } from './ui/button'
 // above user all the session data available --> check docs
 
 // when ever you see use keyword ussai direct data nhi lia ja skta and that is hook method 
@@ -16,13 +17,26 @@ function Navbar() {
     const {data:session} = useSession()
     //from data only bass session active hai ki nhi woo ptta chalta hai
 
-    const user: User = session?.user
+    const user: User = session?.user as User
 
   return (
-    <nav>
-        <div>
-           <a href="">Mystry Message</a>
-           
+    <nav className='p-4 md:p-6 shadow-md'>
+        <div className='container mx-auto flex flex-col md:flex-row justify-between items-center'>
+           <a className='text-xl font-bold mb-4 md:mb-0' href="">Mystry Message</a>
+           {/* check session now  */}
+
+           {
+            session ? (
+              <>
+              <span className='mr-4'>Welcome, {user?.username || user?.email}</span>
+              <Button  className='w-full md:w-auto' onClick={()=> signOut()}>Logout</Button>
+              </>
+            ) : (
+              <Link  href='/sign-in'>
+                    <Button className='w-full md:w-auto'>Login</Button>
+              </Link>
+            )
+           }
         </div>
     </nav>
   )
